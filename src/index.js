fbf050bc7ff9dff20f9cec1e73d58339940568bc
AJ Guiles <ashiguiles@gmail.com>
2019-10-19 17:56:18 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let  taskForm = document.getElementById("create-task-form");
+  let  newTaskDescription = document.getElementById("new-task-description");
+  
+
+  const taskUl = document.getElementById("tasks");
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+
+  taskForm.addEventListener("submit", function(e) {
+    if (e.preventDefault);
+    taskList.createNewTask(newTaskDescription.value);
+    e.target.reset();
+    renderApp();
+  });
+
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+
+  });
+   
 });
+
+
